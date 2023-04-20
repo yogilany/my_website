938f5e0bfdd801d1454014c257f4b96527483e8d
Yousef Gilany <yogilany@gmail.com>
2023-04-20 09:01:13 +0200

    ui update

--- src/pages/MainPage/components/Projects/Projects.jsx
+++ src/pages/MainPage/components/Projects/Projects.jsx
@@ -4,15 +4,14 @@ const Projects = () => {
   return (
     <section class=" bg-gray-800 py-12 px-3 ">
       <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-        <h1 class=" text-3xl font-extrabold text-gray-900 text-white md:text-5xl lg:text-6xl">
+        <h1 class=" text-3xl font-extrabold  text-white md:text-5xl lg:text-6xl">
           <span class="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">
             Some
           </span>{" "}
-          Previous projects{" "}
+          Previous projects
         </h1>
-        <h1 class="mb-16 text-3xl font-extrabold text-gray-900 text-white md:text-5xl lg:text-6xl">
-          <small class="ml-2 font-semibold text-gray-500 text-gray-400">
-            {" "}
+        <h1 class="mb-16 text-3xl font-extrabold  text-white md:text-5xl lg:text-6xl">
+          <small class="ml-2 font-semibold  text-gray-400">
             but I am eager to take on more.
           </small>
         </h1>
@@ -36,28 +35,28 @@ const Projects = () => {
             </span>
             <h3 class="mb-1 text-lg font-semibold  text-white">
               Eventbrite application clone
-              <small class="ml-2 font-semibold text-gray-500 text-gray-400">
+              <small class="ml-2 font-semibold  text-gray-400">
                 <br />
                 In Progress
               </small>
             </h3>
 
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 text-gray-500">
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">
               A third-year college project. | Software Engineering course
               project
             </time>
-            <p class="mb-4 text-base font-normal text-gray-500 text-gray-400">
+            <p class="mb-4 text-base font-normal  text-gray-400">
               Description: The project aims to design and implement a software
               product using state-of-the-art tools and technologies in the
               software industry. I am the subteam leader of the frontend team.
             </p>
-            <p class="mb-4 text-base font-normal text-gray-500 text-gray-400">
+            <p class="mb-4 text-base font-normal  text-gray-400">
               Tools and Technologies: React, Redux, React-Router, React
               Bootstrap, Material UI, Axios
             </p>
             {/* <a
               href="#"
-              class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700"
+              class="inline-flex items-center px-4 py-2 text-sm font-medium  bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700"
             >
               <svg
                 class="w-4 h-4 mr-2"
@@ -75,7 +74,7 @@ const Projects = () => {
             </a> */}
           </li>
           <li class="mb-10 ml-6">
-            <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white ring-gray-900 bg-blue-900">
+            <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8  ring-gray-900 bg-blue-900">
               <svg
                 aria-hidden="true"
                 class="w-3 h-3 text-blue-800 text-blue-300"
@@ -90,31 +89,31 @@ const Projects = () => {
                 ></path>
               </svg>
             </span>
-            <h3 class="mb-1 text-lg font-semibold text-gray-900 text-white">
+            <h3 class="mb-1 text-lg font-semibold  text-white">
               WAREHOUSE MANAGEMENT SYSTEM
-              <small class="ml-2 font-semibold text-gray-500 text-gray-400">
+              <small class="ml-2 font-semibold  text-gray-400">
                 <br />
                 Jan 2022{" "}
               </small>
             </h3>
 
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 text-gray-500">
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">
               A second-year college project. | Database Management course
               project{" "}
             </time>
-            <p class="text-base font-normal text-gray-500 text-gray-400">
+            <p class="text-base font-normal  text-gray-400">
               Description: The system is designed for a warehouse with multiple
               storefronts. It enables the workers to monitor the availability
               and location of all goods, tracks orders from suppliers, and
               adding analytic tools for top managers
             </p>
-            <p class="text-base font-normal text-gray-500 text-gray-400">
+            <p class="text-base font-normal  text-gray-400">
               Tools and Technologies: C#, Microsoft SQL Server Management,
               MySQL.
             </p>
           </li>
           <li class="ml-6 mb-10 ">
-            <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white ring-gray-900 bg-blue-900">
+            <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8  ring-gray-900 bg-blue-900">
               <svg
                 aria-hidden="true"
                 class="w-3 h-3 text-blue-800 text-blue-300"
@@ -129,30 +128,30 @@ const Projects = () => {
                 ></path>
               </svg>
             </span>
-            <h3 class="mb-1 text-lg font-semibold text-gray-900 text-white">
+            <h3 class="mb-1 text-lg font-semibold  text-white">
               MARS EXPLORATION
-              <small class="ml-2 font-semibold text-gray-500 text-gray-400">
+              <small class="ml-2 font-semibold  text-gray-400">
                 <br />
                 Jan 2022{" "}
               </small>
             </h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 text-gray-500">
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">
               A second-year college project. | Data Structures and Algorithms
               course project{" "}
             </time>
-            <p class="text-base font-normal text-gray-500 text-gray-400">
+            <p class="text-base font-normal  text-gray-400">
               Description: A simple command-line simulator for a hypothetical
               Mars exploaration mission. It gets from the user information about
               the rovers and the missions required, Then it will simulates the
               mission assignment process
             </p>
-            <p class="text-base font-normal text-gray-500 text-gray-400">
+            <p class="text-base font-normal  text-gray-400">
               Tools and Technologies: C++, Data Structures, Microsoft Visual
               Studio 2019
             </p>
           </li>
           <li class="ml-6 mb-10 ">
-            <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white ring-gray-900 bg-blue-900">
+            <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8  ring-gray-900 bg-blue-900">
               <svg
                 aria-hidden="true"
                 class="w-3 h-3 text-blue-800 text-blue-300"
@@ -167,29 +166,29 @@ const Projects = () => {
                 ></path>
               </svg>
             </span>
-            <h3 class="mb-1 text-lg font-semibold text-gray-900 text-white">
+            <h3 class="mb-1 text-lg font-semibold  text-white">
               PAINT FOR KIDS GAME APP
-              <small class="ml-2 font-semibold text-gray-500 text-gray-400">
+              <small class="ml-2 font-semibold  text-gray-400">
                 <br />
                 Jun 2021{" "}
               </small>
             </h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 text-gray-500">
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">
               A first-year college project. | Programming Techniques course
               project,
             </time>
-            <p class="text-base font-normal text-gray-500 text-gray-400">
+            <p class="text-base font-normal  text-gray-400">
               Description: Applying object oriented programming concepts. we
               built a simple application that enables kids to draw fancy shapes
               and also play some simple games with those shapes
             </p>
-            <p class="text-base font-normal text-gray-500 text-gray-400">
+            <p class="text-base font-normal  text-gray-400">
               Tools and Technologies: C++, object oriented programming,
               Microsoft Visual Studio 2019
             </p>
           </li>
           <li class="ml-6 mb-10 ">
-            <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white ring-gray-900 bg-blue-900">
+            <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8  ring-gray-900 bg-blue-900">
               <svg
                 aria-hidden="true"
                 class="w-3 h-3 text-blue-800 text-blue-300"
@@ -204,28 +203,28 @@ const Projects = () => {
                 ></path>
               </svg>
             </span>
-            <h3 class="mb-1 text-lg font-semibold text-gray-900 text-white">
+            <h3 class="mb-1 text-lg font-semibold  text-white">
               THE PROCESSOR SIMULATION GAME
-              <small class="ml-2 font-semibold text-gray-500 text-gray-400">
+              <small class="ml-2 font-semibold  text-gray-400">
                 <br />
                 Jan 2022{" "}
               </small>
             </h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 text-gray-500">
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">
               A second-year college project. | Microprocessors-1 course project{" "}
             </time>
-            <p class="text-base font-normal text-gray-500 text-gray-400">
+            <p class="text-base font-normal  text-gray-400">
               Description: A two-player processor simulation where each player
               tries to prevent their opponent from reaching a specific value in
               one of their registers. The players communicate with each other
               using serial communications.
             </p>
-            <p class="text-base font-normal text-gray-500 text-gray-400">
+            <p class="text-base font-normal  text-gray-400">
               Tools and Technologies: Intel x86 Assembly, DOSBox, MASM.
             </p>
           </li>
           <li class="ml-6 mb-10 ">
-            <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white ring-gray-900 bg-blue-900">
+            <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8  ring-gray-900 bg-blue-900">
               <svg
                 aria-hidden="true"
                 class="w-3 h-3 text-blue-800 text-blue-300"
@@ -240,23 +239,23 @@ const Projects = () => {
                 ></path>
               </svg>
             </span>
-            <h3 class="mb-1 text-lg font-semibold text-gray-900 text-white">
+            <h3 class="mb-1 text-lg font-semibold  text-white">
               OPERATING SYSTEM SCHEDULER SIMULATO
-              <small class="ml-2 font-semibold text-gray-500 text-gray-400">
+              <small class="ml-2 font-semibold  text-gray-400">
                 <br />
                 Jan 2023{" "}
               </small>
             </h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 text-gray-500">
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">
               A third-year college project. | Operating Systems.
             </time>
-            <p class="text-base font-normal text-gray-500 text-gray-400">
+            <p class="text-base font-normal  text-gray-400">
               Description: A CPU scheduler determines an order for the execution
               of its processes according to a chosen scheduling algorithms:
               Highest Priority First, Shortest Time Remaining Next, Round Robin,
               and Multi Level Queue
             </p>
-            <p class="text-base font-normal text-gray-500 text-gray-400">
+            <p class="text-base font-normal  text-gray-400">
               Tools and Technologies: C, Linux, Docker
             </p>
           </li>
