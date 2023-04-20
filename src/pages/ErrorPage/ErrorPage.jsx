cedd178a783f1e4f009689c066e7af2c5a8f44a5
Yousef Gilany <yogilany@gmail.com>
2023-04-20 09:08:38 +0200

    update error

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ErrorPage = () => {
   return (
-    <section class="bg-white bg-gray-900">
+    <section class=" bg-gray-900 min-h-screen">
       <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div class="mx-auto max-w-screen-sm text-center">
           <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 text-primary-500">
